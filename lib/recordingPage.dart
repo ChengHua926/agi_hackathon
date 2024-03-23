2269f3690b15066ccd347cacb201b1a7a2d8d649
ChengHua926 <25chenghua@gmail.com>
2024-03-23 19:49:30 +0800

    switch between two pages

--- lib/recordingPage.dart
+++ lib/recordingPage.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,43 +13,48 @@ class RecordingPage extends StatefulWidget {
 
 class _RecordingPageState extends State<RecordingPage> {
   final _formKey = GlobalKey<FormState>();
-  String? _category =
-      '餐饮'; // Default value to match your dropdown's initial value
+  String? _category;
   double _amount = 0.0;
-  final List<String> _categories = ['餐饮', '购物', '出行', '社交', '其他'];
+  List<String> _categories = ['餐饮', '购物', '出行', '社交', '其他'];
   final TextEditingController _amountController = TextEditingController();
-
-  // State management for FAB icon and speech listening status
+  bool _isExpense = true; // Track if it's an expense or income page
   bool _isListening = false;
-  IconData _fabIcon = Icons.mic;
+  IconData _micIcon = Icons.mic;
+  bool _isLoading = false;
+  Color _themeColor = Colors.deepOrange; // Default color for expenses
 
   @override
   void initState() {
     super.initState();
-    // Initialize the TextEditingController with the default amount
+    _category = _categories.first;
     _amountController.text = _amount.toString();
   }
 
   @override
   void dispose() {
-    // Dispose of the controller when the widget is removed from the widget tree
     _amountController.dispose();
     super.dispose();
   }
 
   void _toggleListening() {
-    setState(() {
-      _isListening = !_isListening;
-      _fabIcon = _isListening ? Icons.mic_off : Icons.mic;
-
-      // Update the form fields when toggling
-      if (!_isListening) {
-        _category = '购物';
-        _amount = 243.0;
-        _amountController.text = _amount.toString();
-      }
-      // Optional: add logic here if you want to reset or change the values when the button is toggled off
-    });
+    if (!_isListening) {
+      setState(() {
+        _isListening = true;
+        _micIcon = Icons.mic_off;
+      });
+    } else {
+      setState(() => _isLoading = true);
+      Future.delayed(const Duration(seconds: 1), () {
+        setState(() {
+          _isListening = false;
+          _micIcon = Icons.mic;
+          _category = '购物';
+          _amount = 243.0;
+          _amountController.text = _amount.toString();
+          _isLoading = false;
+        });
+      });
+    }
   }
 
   void _submitData() {
@@ -56,87 +62,192 @@ class _RecordingPageState extends State<RecordingPage> {
       _formKey.currentState!.save();
       Provider.of<SpendingListProvider>(context, listen: false)
           .addSpending(_category!, _amount);
-      Navigator.pop(context); // Pop the current route after recording data
+      Navigator.pop(context);
     }
+  }
+
+  void _togglePage() {
+    setState(() {
+      _isExpense = !_isExpense;
+      _category = null;
+      _amount = 0.0;
+      _amountController.text = _amount.toString();
+      _categories = _isExpense
+          ? ['餐饮', '购物', '出行', '社交', '其他']
+          : ['工资', '理财', '礼金', '其他'];
+      _themeColor = _isExpense ? Colors.deepOrange : Colors.teal[800]!;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Record Spending')),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    DropdownButtonFormField<String>(
-                      value: _category,
-                      decoration: const InputDecoration(labelText: 'Category'),
-                      items: _categories
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          _category = newValue;
-                        });
-                      },
-                      onSaved: (value) => _category = value,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please select a category';
-                        }
-                        return null;
-                      },
+      appBar: AppBar(
+        title: Text(_isExpense ? '记录支出' : '记录收入'),
+        actions: [
+          IconButton(
+            icon: Icon(_isExpense ? Icons.inbox : Icons.outbox),
+            onPressed: _togglePage,
+          ),
+        ],
+        backgroundColor: _themeColor,
+      ),
+      body: Stack(
+        children: [
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Card(
+                    elevation: 4.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    TextFormField(
-                      decoration: const InputDecoration(labelText: 'Amount'),
-                      controller: _amountController, // Use the controller here
-
-                      keyboardType: TextInputType.number,
-                      onSaved: (value) =>
-                          _amount = double.tryParse(value ?? '0.0') ?? 0.0,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter an amount';
-                        }
-                        if (double.tryParse(value) == null) {
-                          return 'Please enter a valid number';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 20),
-                    Center(
-                      child: ElevatedButton(
-                        onPressed: _submitData,
-                        child: const Text('Done'),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            _isExpense ? "支出" : "收入",
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              color: _themeColor,
+                            ),
+                          ),
+                          const SizedBox(height: 16.0),
+                          Form(
+                            key: _formKey,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                DropdownButtonFormField<String>(
+                                  value: _category,
+                                  decoration: InputDecoration(
+                                    labelText: '类别',
+                                    labelStyle: TextStyle(
+                                      color: _themeColor,
+                                    ),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                  items: _categories.map<DropdownMenuItem<String>>((String value) {
+                                    return DropdownMenuItem<String>(
+                                      value: value,
+                                      child: Text(value),
+                                    );
+                                  }).toList(),
+                                  onChanged: (String? newValue) {
+                                    setState(() {
+                                      _category = newValue;
+                                    });
+                                  },
+                                  onSaved: (value) => _category = value,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return '请选择一个类别';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                const SizedBox(height: 16.0),
+                                TextFormField(
+                                  decoration: InputDecoration(
+                                    labelText: '金额',
+                                    labelStyle: TextStyle(
+                                      color: _themeColor,
+                                    ),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                  controller: _amountController,
+                                  keyboardType: TextInputType.number,
+                                  onSaved: (value) => _amount = double.tryParse(value ?? '0.0') ?? 0.0,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return '请输入金额';
+                                    }
+                                    if (double.tryParse(value) == null) {
+                                      return '请输入有效的数字';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                const SizedBox(height: 16.0),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    ElevatedButton(
+                                      onPressed: _submitData,
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: _themeColor,
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 32.0,
+                                          vertical: 16.0,
+                                        ),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(10.0),
+                                        ),
+                                      ),
+                                      child: const Text(
+                                        '完成',
+                                        style: TextStyle(fontSize: 18.0, color: Colors.white),
+                                      ),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: _toggleListening,
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: _themeColor,
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 32.0,
+                                          vertical: 16.0,
+                                        ),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(10.0),
+                                        ),
+                                      ),
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Icon(_micIcon, color: Colors.white),
+                                          const SizedBox(width: 8.0),
+                                          Text(
+                                            _isListening ? '停止录音' : '开始录音',
+                                            style: const TextStyle(fontSize: 16.0, color: Colors.white),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: FloatingActionButton(
-                    onPressed: _toggleListening,
-                    child: Icon(_fabIcon),
-                    elevation: 4.0,
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
+          _isLoading ? _loadingOverlay() : const SizedBox.shrink(),
+        ],
+      ),
+    );
+  }
+
+  Widget _loadingOverlay() {
+    return Container(
+      color: Colors.black.withOpacity(0.5),
+      child: Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(_themeColor),
         ),
       ),
     );
